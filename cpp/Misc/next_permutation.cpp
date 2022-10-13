a4764283bd398a1f75e705bb658e67d33e569e2f
Victor Manuel <mbvictorsantos@gmail.com>
2022-10-13 16:28:40 -0300

    fix: Move comentário do next_permutation

--- cpp/Misc/next_permutation.cpp
+++ cpp/Misc/next_permutation.cpp
@@ -1,9 +1,11 @@
 vector<int> arr = {1, 2, 3};
 int n = arr.size();
 
-do { // 1,2,3; 1,3,2; 2,1,3; 2,3,1; 3,1,2; 3,2,1; 
+do {
     for (auto e : arr) {
         cout << e << ' ';
     }
     cout << '\n';
-} while (next_permutation(arr.begin(), arr.begin() + n)); 
+} while (next_permutation(arr.begin(), arr.end()));
+
+// output: 1,2,3; 1,3,2; 2,1,3; 2,3,1; 3,1,2; 3,2,1; 
